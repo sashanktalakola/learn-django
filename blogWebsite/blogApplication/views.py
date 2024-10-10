98cb086727adb5b4d7f401751e3b69c08fd96c82
Sashank <sashanktalakola2@gmail.com>
2024-10-10 14:03:09 -0400

    made homepage content dynamic

--- blogWebsite/blogApplication/views.py
+++ blogWebsite/blogApplication/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.conf import settings
+import json
 
 # Create your views here.
 def index(request):
-    return render(request, "blogApplication/index.html")
+
+    dataPath = f"{settings.BASE_DIR}/data/index.json"
+
+    with open(dataPath) as f:
+        pageData = json.load(f)
+    
+    featuredArticlesData = pageData["featured-articles"]
+    allArticlesData = pageData["all-articles"]
+
+    return render(request, "blogApplication/index.html", {
+        "featuredArticles": featuredArticlesData,
+        "allArticles": allArticlesData
+    })

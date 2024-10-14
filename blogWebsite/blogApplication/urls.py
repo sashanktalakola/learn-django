a4ebc98db8d3f409b07331523991351dd18b4641
Sashank <sashanktalakola2@gmail.com>
2024-10-14 14:12:59 -0400

    Updated routes

--- blogWebsite/blogApplication/urls.py
+++ blogWebsite/blogApplication/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
-    path("@<str:username>", views.author),
-    path("@<str:username>/<slug:slug>", views.viewPost)
+    path("@<str:username>/", views.author),
+    path("@<str:username>/<slug:slug>/", views.viewPost)
 ]

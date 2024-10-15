49f5bfa82bf30bed580ae1968db2edb736d4adf4
Sashank <sashanktalakola2@gmail.com>
2024-10-14 21:57:15 -0400

    book store application - django models exercise

--- blogApplication/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path("", views.index),
-    path("@<str:username>/", views.author),
-    path("@<str:username>/<slug:slug>/", views.viewPost)
-]
--- books/apps.py
+++ books/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BlogapplicationConfig(AppConfig):
+class BooksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blogApplication'
+    name = 'books'

512fceecc854221fe4ea2a772e450f813a23f754
Sashank <sashanktalakola2@gmail.com>
2024-10-16 14:22:44 -0400

    added User model and updated other models

--- blogApplication/admin.py
+++ blogApplication/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Category, Tag, Post
-from django.contrib.auth.models import User
+from .models import Category, Tag, Post, User
 
 
 # Register your models here.
+admin.site.register(User)
 admin.site.register(Category)
 admin.site.register(Tag)
 admin.site.register(Post)
--- blogApplication/models.py
+++ blogApplication/models.py
@@ -1,7 +1,21 @@
 from django.db import models
-from django.contrib.auth.models import User
 from django.utils.text import slugify
 from .utils import getRandomSlug
+
+
+class User(models.Model):
+    username = models.CharField(max_length=150, unique=True, blank=False, null=False)
+    first_name = models.CharField(max_length=30, blank=False, null=False)
+    last_name = models.CharField(max_length=30, blank=False, null=False)
+    email = models.EmailField(unique=True, blank=False, null=False)
+    password = models.CharField(max_length=128, blank=False, null=False)
+    date_of_birth = models.DateField(blank=False, null=False)
+    profile_picture = models.CharField(max_length=255, blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
 
 
 class Category(models.Model):
